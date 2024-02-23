d8f08008cd05fa15679459fa9731d1a928bab77e
shy <shy@shylinux.com>
2024-02-23 21:25:44 +0800

    opt some

--- portal/development/create/main.js
+++ portal/development/create/main.js
@@ -1,17 +1,44 @@
 Volcanos({river: {
-	product: {name: "产品群", icon: "bi bi-bar-chart-line-fill", storm: {
-		office: {name: "办公 office", icon: "bi bi-bar-chart-line-fill",  index: ["web.wiki.portal", "web.chat.macos.desktop", "web.wiki.feel", "web.wiki.draw", "web.wiki.data"]},
+	product: {name: "产品群", icon: "bi bi-grid-3x3-gap", storm: {
+		portal: {name: "官网 portal", icon: "bi bi-globe", index: [
+			"portal",
+		]},
+		desktop: {name: "桌面 desktop", icon: "bi bi-window-desktop", index: [
+			"desktop",
+		]},
+		dream: {name: "空间 dream", icon: "bi bi-diagram-3", index: [
+			"dream",
+		]},
+		store: {name: "商店 store", icon: "bi bi-shop", index: [
+			"store",
+		]},
 	}},
-	project: {name: "研发群", icon: "bi bi-git", storm: {
-		studio: {name: "研发 studio", icon: "bi bi-git", index: ["web.code.vimer", "web.code.git.status", "web.chat.favor", "web.team.plan", "web.wiki.word"]},
+	project: {name: "研发群", icon: "bi bi-git", type: aaa.TECH, debug: true, storm: {
+		studio: {name: "研发 studio", icon: "bi bi-code-slash", index: [
+			"vimer", "status", "dream", {index: "web.chat.script", args: ["devops"]}, "favor", "word",
+		]},
+		repos: {name: "源码 repos", icon: "bi bi-git", index: [
+			"compile", "publish", "matrix", "store", "web.code.git.repos", "web.code.git.service",
+		]},
+		stats: {name: "流量 stats", icon: "bi bi-bar-chart-line", index: [
+			"stats", "count", "toast", "broad", "spide", "cache", "space", "route",
+		]},
 	}},
-	profile: {name: "测试群", icon: "bi bi-list-columns", type: aaa.TECH, storm: {
-		release: {name: "发布 release", icon: "bi bi-list-check", index: ["web.code.compile", "web.code.publish", "web.code.pprof", "web.code.bench", "web.dream", "web.space", "web.code.git.service", "web.code.git.status"]},
+	operate: {name: "运维群", icon: "bi bi-shield-lock", type: aaa.TECH, storm: {
+		share: {name: "共享 share", icon: "bi bi-share", index: [
+			"share", "token", "qrcode",
+		]},
+		oauth: {name: "登录 oauth", icon: "bi bi-shield-lock", index: [
+			"header", "web.chat.oauth.client", "web.chat.wx.access", "web.chat.message",
+		]},
+		email: {name: "邮箱 email", icon: "bi bi-envelope", index: [
+			"email", "offer", "apply",
+		]},
+		user: {name: "用户 user", icon: "bi bi-people-fill", index: [
+			"user", "totp", "sess", "role",
+		]},
+		system: {name: "系统 system", icon: "bi bi-ubuntu", index: [
+			"dir", "trash", "host", "port", "mirrors", "runtime", "log.debug",
+		]},
 	}},
-	operate: {name: "运维群", icon: "bi bi-gear", type: aaa.TECH, storm: {
-		web: {name: "应用 web", icon: "bi bi-browser-chrome", index: ["broad", "serve", "space", "dream", "share"]},
-		aaa: {name: "权限 aaa", icon: "bi bi-people-fill", index: ["offer", "email", "user", "totp", "sess", "role"]},
-		cli: {name: "系统 cli", icon: "bi bi-windows", index: ["qrcode", "daemon", "runtime", "cli.procstat", "cli.procinfo", "mirrors", "signal", "timer", "routine", "log.debug"]},
-		nfs: {name: "文件 nfs", icon: "bi bi-server", index: ["dir", "cat", "tar", "pack", "tail", "trash", "server", "host", "port"]},
-	}},
-}, list: ["/require/modules/bootstrap-icons/font/bootstrap-icons.min.css"]}) // https://icons.getbootstrap.com/
+}})
